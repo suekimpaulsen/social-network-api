be24e617caf2790b324ac13f1d26bfa7978973b2
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-09-05 13:43:37 -0500

    user & thought controller notes added

---
+++ controllers/thought-controller.js
@@ -0,0 +1,14 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  // GET all thoughts
+  // GET one thought by id
+  // CREATE new thought -- POST
+  // UPDATE thought by id -- PUT
+  // DELETE thought by id -- DELETE
+
+  // create new reaction for thought
+  // delete reaction from thought
+}
+
+module.exports = thoughtController;
